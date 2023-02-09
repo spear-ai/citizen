0766a8cbc57dfc4f4d1c248b477f2e0e59ecb8dc
Dennis Torres <dtorres@spear.ai>
2023-02-09 17:04:44 -0500

    feat: add a `npm-package-json-lint` config package (#18)

---
+++ packages/npm-package-json-lint-config/src/base.ts
@@ -0,0 +1,24 @@
+const baseNpmPackageJsonLintConfig = {
+  extends: 'npm-package-json-lint-config-default',
+  rules: {
+    'description-format': ['error', {
+      'forbidEndingPeriod': true
+    }],
+    'no-duplicate-properties': 'error',
+    'no-repeated-dependencies': 'error',
+    'prefer-absolute-version-devDependencies': ['error', {}],
+    'prefer-alphabetical-bundledDependencies': 'error',
+    'prefer-alphabetical-dependencies': 'error',
+    'prefer-alphabetical-devDependencies': 'error',
+    'prefer-alphabetical-optionalDependencies': 'error',
+    'prefer-alphabetical-peerDependencies': 'error',
+    'prefer-alphabetical-scripts': 'error',
+    'prefer-property-order': ['error', []],
+    'require-description': 'error',
+    'require-license': 'error',
+    'valid-values-license': ['error', ['MIT', 'UNLICENSED']],
+    'version-format': 'error'
+  }
+}
+
+export default baseNpmPackageJsonLintConfig;
