5f0d3d4f53a688da021102a88476a53903b62698
Dennis Torres <djtorres@protonmail.com>
2023-03-14 16:15:18 -0400

    feat(eslint-config): add react rules to ESLint config

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -1,4 +1,5 @@
 import eslintCommentsPlugin from "@eslint-community/eslint-plugin-eslint-comments";
+import nextPlugin from "@next/eslint-plugin-next";
 import typescriptPlugin from "@typescript-eslint/eslint-plugin";
 import typescriptParser from "@typescript-eslint/parser";
 import type { ESLint, Linter } from "eslint";
@@ -11,14 +12,22 @@ import airbnbBaseConfigStrict from "eslint-config-airbnb-base/rules/strict";
 import airbnbBaseConfigStyle from "eslint-config-airbnb-base/rules/style";
 import airbnbBaseConfigVariables from "eslint-config-airbnb-base/rules/variables";
 import airbnbBaseTypescriptConfig from "eslint-config-airbnb-typescript/lib/shared";
+import airbnbConfigReact from "eslint-config-airbnb/rules/react";
+import airbnbConfigReactA11y from "eslint-config-airbnb/rules/react-a11y";
+import airbnbConfigReactHooks from "eslint-config-airbnb/rules/react-hooks";
 import arrayFuncPlugin from "eslint-plugin-array-func";
 import formatJsPlugin from "eslint-plugin-formatjs";
 import importPlugin from "eslint-plugin-import";
 import jsoncPlugin from "eslint-plugin-jsonc";
+import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 import promisePlugin from "eslint-plugin-promise";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import reactUsePropsPlugin from "eslint-plugin-react-use-props";
 import regexpPlugin from "eslint-plugin-regexp";
 import sonarjsPlugin from "eslint-plugin-sonarjs";
 import sortDestructureKeysPlugin from "eslint-plugin-sort-destructure-keys";
+import tailwindCssPlugin from "eslint-plugin-tailwindcss";
 import tomlPlugin from "eslint-plugin-toml";
 import yamlPlugin from "eslint-plugin-yml";
 import typescriptSortKeysPlugin from "eslint-plugin-typescript-sort-keys";
@@ -100,6 +109,7 @@ export const jsonFamilyRules: Linter.RulesRecord = {
 
 export const allowedAbbreviations = {
   "eslint-plugin-array-func.d": true,
+  "eslint-plugin-react-use-props.d": true,
   "next-env.d": true,
   "next-env.override.d": true,
 };
@@ -220,6 +230,7 @@ export const eslintConfig: Linter.FlatConfig[] = [
     files: [
       "**/*.cjs",
       "**/*.cts",
+      "**/*.d.ts",
       "**/*.js",
       "**/*.jsx",
       "**/*.mjs",
@@ -237,14 +248,20 @@ export const eslintConfig: Linter.FlatConfig[] = [
     },
     plugins: {
       "@eslint-community/eslint-comments": eslintCommentsPlugin,
+      "@next/next": nextPlugin,
       "@typescript-eslint": typescriptPlugin,
       "array-func": arrayFuncPlugin,
       "formatjs": formatJsPlugin,
       "import": importPlugin,
+      "jsx-a11y": jsxA11yPlugin,
       "promise": promisePlugin,
+      "react": reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "react-use-props": reactUsePropsPlugin,
       "regexp": regexpPlugin,
       "sonarjs": sonarjsPlugin as ESLint.Plugin,
       "sort-destructure-keys": sortDestructureKeysPlugin,
+      "tailwindcss": tailwindCssPlugin,
       "typescript-sort-keys": typescriptSortKeysPlugin,
       "unicorn": unicornPlugin,
     },
@@ -258,6 +275,11 @@ export const eslintConfig: Linter.FlatConfig[] = [
       ...airbnbBaseConfigImports.rules,
       ...airbnbBaseConfigStrict.rules,
       ...airbnbBaseTypescriptConfig.rules,
+      ...airbnbConfigReact.rules,
+      ...airbnbConfigReactA11y.rules,
+      ...airbnbConfigReactHooks.rules,
+      ...nextPlugin.configs!.recommended.rules,
+      ...nextPlugin.configs!["core-web-vitals"].rules,
       ...typescriptPlugin.configs!.base.rules,
       ...typescriptPlugin.configs!["eslint-recommended"]!.overrides![0].rules,
       ...typescriptPlugin.configs!["recommended-requiring-type-checking"].rules,
@@ -269,11 +291,13 @@ export const eslintConfig: Linter.FlatConfig[] = [
       ...regexpPlugin.configs!.all.rules,
       ...promisePlugin.configs!.recommended.rules,
       ...sonarjsPlugin.configs.recommended.rules,
+      ...tailwindCssPlugin.configs!.recommended.rules,
       ...typescriptSortKeysPlugin.configs!.recommended.rules,
       ...unicornPlugin.configs!.recommended.rules,
       "@eslint-community/eslint-comments/disable-enable-pair": ["error", {
         allowWholeFile: true,
       }],
+      "@next/next/google-font-display": ["off"],
       "@typescript-eslint/quotes": ["error", "double", { avoidEscape: true }],
       "array-func/prefer-array-from": ["off"],
       "formatjs/enforce-default-message": ["error"],
@@ -287,6 +311,15 @@ export const eslintConfig: Linter.FlatConfig[] = [
       "import/no-anonymous-default-export": ["error"],
       "import/no-default-export": ["error"],
       "import/prefer-default-export": ["off"],
+      "jsx-a11y/anchor-is-valid": ["error", {
+        aspects: ["invalidHref", "preferButton"],
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+      }],
+      "jsx-a11y/label-has-associated-control": ["error", {
+        assert: "either",
+      }],
+      "jsx-a11y/no-autofocus": ["off"],
       "max-len": ["error", {
         code: 120,
         ignoreComments: false,
@@ -325,7 +358,28 @@ export const eslintConfig: Linter.FlatConfig[] = [
         },
       }],
       "quote-props": ["error", "consistent-as-needed"],
+      "react/forbid-component-props": ["error", {
+        forbid: [{
+          allowedFor: ["FormattedList", "FormattedNumber"],
+          message: "Use the `className` property instead",
+          propName: "style",
+        }],
+      }],
+      "react/forbid-dom-props": ["error", {
+        forbid: [{
+          message: "Use the `className` property instead",
+          propName: "style",
+        }],
+      }],
+      "react/function-component-definition": ["error", {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      }],
+      "react/jsx-sort-props": ["error"],
+      "react/style-prop-object": ["off"],
+      "react-hooks/exhaustive-deps": ["off"],
       "sonarjs/cognitive-complexity": ["off"],
+      "sonarjs/no-duplicate-string": ["off"],
       "sort-destructure-keys/sort-destructure-keys": ["error"],
       "sort-imports": ["error", {
         ignoreDeclarationSort: true,
@@ -334,6 +388,12 @@ export const eslintConfig: Linter.FlatConfig[] = [
         natural: true,
       }],
       "sort-vars": ["error"],
+      "tailwindcss/classnames-order": ["error"],
+      "tailwindcss/enforces-negative-arbitrary-values": ["error"],
+      "tailwindcss/enforces-shorthand": ["error"],
+      "tailwindcss/migration-from-tailwind-2": ["error"],
+      "tailwindcss/no-arbitrary-value": ["error"],
+      "tailwindcss/no-custom-classname": ["error"],
       "unicorn/custom-error-definition": ["error"],
       "unicorn/no-null": ["off"],
       "unicorn/prefer-at": ["error"],
@@ -350,6 +410,7 @@ export const eslintConfig: Linter.FlatConfig[] = [
     settings: {
       ...airbnbBaseConfigImports.settings,
       ...airbnbBaseTypescriptConfig.settings,
+      ...airbnbConfigReact.settings,
       ...typescriptPlugin.configs!["recommended-requiring-type-checking"].settings,
     },
   },
---
+++ packages/eslint-config/types/eslint-config-airbnb.d.ts
@@ -0,0 +1,17 @@
+declare module "eslint-config-airbnb/rules/react" {
+  type Config = import("eslint").Linter.Config;
+  const config: Config;
+  export default config;
+}
+
+declare module "eslint-config-airbnb/rules/react-a11y" {
+  type Config = import("eslint").Linter.Config;
+  const config: Config;
+  export default config;
+}
+
+declare module "eslint-config-airbnb/rules/react-hooks" {
+  type Config = import("eslint").Linter.Config;
+  const config: Config;
+  export default config;
+}
