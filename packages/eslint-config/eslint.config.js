fa1bf05b3ec900c0b8525d87c036174054ce1c51
Dennis Torres <djtorres@protonmail.com>
2023-03-16 17:08:11 -0400

    chore: wip

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -1,14 +1,14 @@
 /* eslint-disable import/no-default-export */
 /* eslint-disable import/no-extraneous-dependencies */
 
-import { eslintConfig as spearEslintConfig } from "@spear-ai/eslint-config";
+import { baseEslintConfig } from "@spear-ai/eslint-config";
 
 /** @type {import("eslint").Linter.FlatConfig} */
 const eslintConfig = [
   {
     ignores: ["dist", "node_modules"],
   },
-  ...spearEslintConfig,
+  ...baseEslintConfig,
 ];
 
 export default eslintConfig;
