1b33e760d7a3513e5e63ed5540ce3a4190cb31a6
Dennis Torres <dtorres@spear.ai>
2023-07-12 19:19:46 -0400

    feat(eslint-config): remove unused rules in dev/config files

---
+++ .changeset/smooth-swans-retire.md
@@ -0,0 +1,5 @@
+---
+"@spear-ai/eslint-config": major
+---
+
+Disabled ESLint `import/no-extraneous-dependencies` rule in dev files.
--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -555,8 +555,13 @@ export const baseEslintConfig: Linter.FlatConfig[] = [
   {
     files: ["**/*.config.*", "**/*.test.*", "**/__test__/**"],
     rules: {
-      "import/no-default-export": ["off"],
       "import/no-extraneous-dependencies": ["off"],
+    },
+  },
+  {
+    files: ["**/*.config.*"],
+    rules: {
+      "import/no-default-export": ["off"],
     },
   },
   {
