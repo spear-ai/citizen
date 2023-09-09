0c85c9457cf50c9f9cedac374b00f5c562eb6e39
Dennis Torres <dtorres@spear.ai>
2023-09-09 14:12:56 -0400

    fix(eslint-config): re-enforce no-unused disable comments

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -1,6 +1,3 @@
-/* eslint-disable import/no-default-export */
-/* eslint-disable import/no-extraneous-dependencies */
-
 import { baseEslintConfig, prettierConfig } from "@spear-ai/eslint-config";
 
 /** @type {import("eslint").Linter.FlatConfig} */
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
 const prettierConfig = require("@spear-ai/prettier-config");
 
 module.exports = prettierConfig;
