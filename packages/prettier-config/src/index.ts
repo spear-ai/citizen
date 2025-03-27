7c12725d71e65098bc84c156020b68802ab0461a
Dennis Torres <dtorres@spear.ai>
2025-03-27 04:52:38 -0400

    Add custom Tailwind functions to Prettier config

--- packages/prettier-config/src/index.ts
+++ packages/prettier-config/src/index.ts
@@ -5,6 +5,7 @@ import type { Config } from "prettier";
 const prettierConfig: Config = {
   plugins: ["prettier-plugin-tailwindcss"],
   printWidth: 109,
+  tailwindFunctions: ["cx"],
 };
 
 export default prettierConfig;
