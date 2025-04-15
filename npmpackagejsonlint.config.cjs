a8e8c437f54bb18148a2e73b27b29413945d8216
Dennis Torres <dtorres@spear.ai>
2025-04-15 09:42:55 -0400

    Enforce pinned NPM package dependencies

--- npmpackagejsonlint.config.cjs
+++ npmpackagejsonlint.config.cjs
@@ -1,8 +1,12 @@
 module.exports = {
   extends: ["@spear-ai/npm-package-json-lint-config/spear-application"],
   rules: {
-    "prefer-absolute-version-dependencies": "off",
-    "prefer-absolute-version-devDependencies": "off",
+    "prefer-absolute-version-dependencies": [
+      "error",
+      {
+        exceptions: ["@spear-ai/eslint-config", "@spear-ai/npm-package-json-lint-config"],
+      },
+    ],
     "prefer-alphabetical-scripts": "off",
   },
 };
